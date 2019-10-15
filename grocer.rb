22846c6f1c83adb9d7fdb7eb66fed1273cf55a0d
Allison Johnson <allison.cuttler@gmail.com>
2019-10-15 17:56:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ def apply_coupons(cart, coupons)
       item_info[:count] -= coupon[:num]
     end #if
   end #each
-  cart.delete_if{|item_info| item_info[:count] <= 0}
+  #cart.delete_if{|item_info| item_info[:count] <= 0}
   cart
 end #method apply_coupons
 
